7673389468790bbcef85146e08b65a802323382d
archiijs <arcauniex@gmail.com>
2016-09-02 13:56:08 +0300

    test priekstelpa static address

--- Priekstelpa/Priekstelpa.ino
+++ Priekstelpa/Priekstelpa.ino
@@ -1,140 +1,148 @@
-/**************************************************************************
-   Souliss - LYT8266 WiFi RGBW LED Bulb
-    
-    This is the basic sketch for an LYT8266 a WiFi LED Bulb based on ESP8266,
-    at first boot it starts as access point with a basic web interface to 
-    allow WiFi and Souliss configuration.
-    Info are stored in the module and used at next boot.
-    
-    Configure one module as Gateway for your network and all the other as 
-    Peer. Before load this sketch, run the e02_LYT8266_WiFi_Erase to erase
-    the memory.
-    Once configured, can be accessed via SoulissApp or openHAB.
- 
-***************************************************************************/
+#include <WiFiClient.h>
+#include <EEPROM.h>
+#include <ESP8266mDNS.h>
+#include <WiFiManager.h>
 
+WiFiManager wifi(0);
+/**************************************************************************
+    Souliss - Espressif ESP8266-Esp12
+   
+    This is the Souliss Gateway, it has DHT11 for Temp & Hum, and PIR sensor
+    for Main Antitheft System, add a blink led for a sign if the Peer Node is 
+    connected to wifi network.
+    
+***************************************************************************/
 // Let the IDE point to the Souliss framework
 #include "SoulissFramework.h"
 
-#include <ESP8266WiFi.h>
-#include <ESP8266WebServer.h>
-#include <ESP8266mDNS.h>
-#include <EEPROM.h>
-#include <WiFiUdp.h>
-
-// Configure the Souliss framework
-#include "bconf/LYT8266_LEDBulb.h"          // Load the code directly on the ESP8266
-#include "conf/RuntimeGateway.h"            // This node is a Peer and can became a Gateway at runtime
-#include "conf/DynamicAddressing.h"         // Use dynamically assigned addresses
-#include "conf/WEBCONFinterface.h"          // Enable the WebConfig interface
-
-
-/*** All configuration includes should be above this line ***/ 
+// **** Define the WiFi name and password ****
+#define WIFICONF_INSKETCH
+#define WiFi_SSID               "mywifi"
+#define WiFi_Password           "mypassword"  
+ 
+// Configure the framework
+#include "bconf/MCU_ESP8266.h"              // Load the code directly on the ESP8266
+#include "conf/IPBroadcast.h"
 #include "Souliss.h"
-   
-// Define logic slots, multicolor lights use four slots
-#define LYTLIGHT1           0    
+#include <DHT.h>
 
-#define RED_STARTUP         0x50
-#define GREEN_STARTUP       0x10
-#define BLUE_STARTUP        0x00
+//PIN DHT
+#define DHTPIN    13                           // Used GPI4 as digital input from dht11
 
-// Setup the libraries for Over The Air Update
-OTA_Setup();
+//PIN for Blink as sign if the node is running
+#define BLINK     5
 
-void setup()
-{
-    // Init the network stack and the bulb, turn on with a warm amber
-    Initialize();
-    InitLYT();
-    
-    /****
-        Generally set a PWM output before the connection will lead the 
-        ESP8266 to reboot for a conflict on the FLASH write access.
-        Here we do the configuration during the WebConfig and so we don't
-        need to write anything in the FLASH, and the module can connect
-        to the last used network.
-    If you don't use the WebConfig use a dummy sketch that connects to
-    your WiFi and then use this sketch
-    ****/
-    SetColor(LYTLIGHT1, RED_STARTUP, GREEN_STARTUP, BLUE_STARTUP);
+// This identify the Slot number of typicals 
+#define TEMPERATURE             0          // This is the memory slot used for DHT11 Temp
+#define HUMIDITY                2          // This is the memory slot used for DHT11 Humidity
+#define DEADBAND                0.01       // Deadband value 1%  
+#define ANTITHEFT               4          // This is the memory slot used for the execution of the anti-theft
+#define WATCHDOG                5
 
-    // Read the IP configuration from the EEPROM, if not available start
-    // the node as access point.
-    //
-    // If you want to force the device in WebConfiguration mode, power OFF
-    // your router and power OFF and then ON the bulb, you will see an access
-    // point called Souliss.
-    if(!ReadIPConfiguration()) 
-    {   
-        // Pulse a bit
-        LYTPulse();
+// Setup the DHT sensor
+DHT dht(DHTPIN, DHT21, 15);
 
-        // Start the node as access point with a configuration WebServer
-        SetAccessPoint();
-        startWebServer();
+// Define the network configuration according to your router settings
+uint8_t ip_address[4]  = {192, 168, 1, 41};
+uint8_t subnet_mask[4] = {255, 255, 255, 0};
+uint8_t ip_gateway[4]  = {192, 168, 1, 1};
 
-        // We have nothing more than the WebServer for the configuration
-        // to run, once configured the node will quit this.
-        while(1)
-        {
-            yield();
-            runWebServer(); 
-        }
 
-    }
+void setup(){  
+    Serial.begin(115200);
+    wifi.autoConnect("Souliss");  
+    WiFi.mode(WIFI_STA);
+        Initialize();
 
-    if (IsRuntimeGateway())
-    {
-        // Connect to the WiFi network and get an address from DHCP                      
-        SetAsGateway(myvNet_dhcp);       // Set this node as gateway for SoulissApp  
-        SetAddressingServer();
-    }
-    else 
-    {
         // This board request an address to the gateway at runtime, no need
         // to configure any parameter here.
-        SetDynamicAddressing();  
-        GetAddress();
-    } 
+        // Set network IP parameters
+        Souliss_SetIPAddress(ip_address, subnet_mask, ip_gateway);
+        SetAsGateway(41);  // Use the last byte of the IP address if using a static IP address
 
-    // Define a logic to handle the bulb
-    SetLYTLamps(LYTLIGHT1);
-    
-    // Init the OTA
-    OTA_Init();
+        SetAddress(0xAB02, 0xFF00, 0x0000); //Antitheft & Temp/Hum esp12                         
+  
+        
+        // Set the typical to use
+        Souliss_SetT52(memory_map, TEMPERATURE);
+        Souliss_SetT53(memory_map, HUMIDITY);
+ 
+        // Setup the anti-theft
+        Set_T41(ANTITHEFT);
+        
+        
+        pinMode(2, INPUT);                 // Use pin GPI02 as digital input for PIR  
+        pinMode(BLINK, OUTPUT);            // use pin 5 as Blink Led
+        dht.begin();
 }
+ 
+void loop(){
 
-void loop()
-{  
-    EXECUTEFAST() {                     
-        UPDATEFAST();   
-        
-        // Is an unusual approach, but to get fast response to color change we run the LYT logic and
-        // basic communication processing at maximum speed.
-        LogicLYTLamps(LYTLIGHT1);       
-        ProcessCommunication();
+           
+            // Here we start to play
+            EXECUTEFAST() {                     
+                UPDATEFAST(); 
+                
+                FAST_50ms(){
+                if (WiFi.status() == WL_CONNECTED) { 
+                digitalWrite(BLINK, !digitalRead(BLINK));
+               
+              }
+                }
+                
+                FAST_510ms(){
+                   // Retreive data from the MaCaco communication channel
+                Souliss_CommunicationData(memory_map, &data_changed);
+               
+                // Compare the acquired input with the stored one, send the new value to the
+                // user interface if the difference is greater than the deadband
+       
+                Souliss_Logic_T52(memory_map, TEMPERATURE, DEADBAND, &data_changed);
+                Souliss_Logic_T53(memory_map, HUMIDITY, DEADBAND, &data_changed);
+                
+                // Input from anti-theft sensor
+                LowDigIn(2, Souliss_T4n_Alarm, ANTITHEFT);
+                   
+                // Execute the anti-theft logic
+                Logic_T41(ANTITHEFT);   
+                   
+                }
+                      
+                // Here we handle here the communication with Android
+                FAST_PeerComms(); 
+                // Execute the code every 2110ms          
+ //               FAST_2110ms()   { 
+             
+              
+                
+                // Build a watchdog chain to monitor the nodes
+ //               mInput(ANTITHEFT) = Watchdog(0xAB03, WATCHDOG, Souliss_T4n_Alarm);                
+ //               }
+         }
+         
+         EXECUTESLOW() {
+         UPDATESLOW();
+                       
+         SLOW_10s() {  
+             // Read temperature value from DHT sensor and convert from single-precision to half-precision
+                    float temperature = dht.readTemperature();
+                    
+                    Souliss_ImportAnalog(memory_map, TEMPERATURE, &temperature);
+                    Serial.print ("Temp  ");
+                    Serial.print (dht.readTemperature());                    
+
+                    // Read humidity value from DHT sensor and convert from single-precision to half-precision
+                    float humidity = dht.readHumidity();
+                    
+                    Souliss_ImportAnalog(memory_map, HUMIDITY, &humidity);
+                    Serial.print (" & Hum  ");    
+                    Serial.println (dht.readHumidity());
+                 
+              
+          } 
+      // Here we periodically check for a gateway to join
+        SLOW_PeerJoin();
+      }
       
-        // Run communication as Gateway or Peer
-        if (IsRuntimeGateway())
-            FAST_GatewayComms(); 
-        else 
-            FAST_PeerComms();   
-    }
-
-    EXECUTESLOW() {
-        UPDATESLOW();
-        
-        // Slowly shut down the lamp
-        SLOW_10s() {
-            LYTSleepTimer(LYTLIGHT1);      
-        }       
-        
-        // If running as Peer
-        if (!IsRuntimeGateway())
-            SLOW_PeerJoin();
-    } 
-    
-    // Look for a new sketch to update over the air
-    OTA_Process();
-}    
+       START_PeerJoin(); //tell gateway that i am exist    
+    }          
